61a18d0a1bc28aeb5bcf0bbe1b743aab1323edc3
pr9nav <133548463+pr9nav@users.noreply.github.com>
2023-05-14 19:52:37 -0400

    Add files via upload

---
+++ 75fa46-2.myshopify.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var parentElement = document.querySelector('#CartDrawer');
+  if (parentElement) {
+    parentElement.addEventListener('click', function(e) {
+      if (e.target.matches('#CartDrawer-Checkout')) {
+        e.preventDefault();
+        fetch('/cart.js')
+          .then(response => response.json())
+          .then(data => {
+            if (data.items.length > 1) {
+              window.location.href = '/checkout';
+            } else if (data.items.length === 1) {
+              var firstItem = data.items[0];
+              var redirectUrl;
+
+              if (firstItem.product_id == '8266162635035') {
+                redirectUrl = 'https://pay.leamoreau.co/hair-extensions/checkout';
+              } else if (firstItem.product_id == '8272355885339') {
+                redirectUrl = 'https://pay.leamoreau.co/trimmer/checkout';
+              }
+              // Add more conditions for additional products as needed
+
+              if (redirectUrl) {
+                var variantName = firstItem.variant_title;
+                window.location.href = redirectUrl + '?variant=' + encodeURIComponent(variantName);
+              } else {
+                window.location.href = '/checkout';
+              }
+            } else {
+              window.location.href = '/checkout';
+            }
+          })
+          .catch(error => console.error('Error:', error));
+      }
+    });
+  }
+});
